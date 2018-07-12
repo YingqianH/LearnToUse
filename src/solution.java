c88d87318eebc4d9c726aef64b4e3d21c0ba10d9
Yingqian Hou <yingqh@amazon.com>
2018-07-12 11:27:48 -0700

    Correct code

--- src/solution.java
+++ src/solution.java
@@ -1,5 +1,5 @@
-import java.util.*;
 public class solution {
-    System.out.println("hello world");
-    //why
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
 }
