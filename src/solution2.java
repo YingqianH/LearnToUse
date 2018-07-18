1b9a32fb275da935b7d8b56aacee12c229d697b2
Yingqian Hou <yingqh@amazon.com>
2018-07-17 20:21:39 -0700

    add comment

--- src/solution2.java
+++ src/solution2.java
@@ -1,10 +1,4 @@
 public static void main(String[] args) {
-    //NV2
-    //NV
-   //print hello world here
    System.out.println("hello");
-   System.out.println("world");
-	//again
 //test git base
-
 }
