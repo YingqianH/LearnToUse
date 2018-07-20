84ddfec95eb0ee7e587eedcd8a0067f5418486ec
Yingqian Hou <yingqh@amazon.com>
2018-07-20 12:02:28 -0700

    first commit message
    create a new java class

--- src/solution.java
+++ src/solution.java
@@ -1,8 +1,6 @@
 public class solution {
     public static void main(String[] args) {
         System.out.println("hello world!");
+	System.out.print();
     }
-}//for NV
-//for NB
-//for test
-//test again
+}
