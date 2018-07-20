abed68bd5ca262a5e4285be1d77b79603999c944
Yingqian Hou <yingqh@amazon.com>
2018-07-20 12:02:28 -0700

    new branch commit

--- src/solution.java
+++ src/solution.java
@@ -1,8 +1,10 @@
 public class solution {
     public static void main(String[] args) {
         System.out.println("hello world!");
+	System.out.print();
     }
 }//for NV
 //for NB
 //for test
 //test again
+
